4c1a5f817c1639e96ac1333bfda226b1d52ec780
Matej Moško <matej.mosko@gmail.com>
2018-08-03 20:42:28 +0200

    TopMenu

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $('document').ready(function() {
-  baseUrl = "http://localhost/~gnaag/panakrala/";
+    baseUrl = "http://localhost/~gnaag/panakrala/";
 
     $('form').ajaxForm({
         target: ".formResult",
@@ -12,15 +12,26 @@ $('document').ready(function() {
     });
 
     function ajaxGetEvent(projectId, eventId) {
-      link = baseUrl+"admin/ajax.php?script=eventGetGuests&eventId="+eventId+"&projectId="+projectId;
-        $.getJSON(link , function(data) {
-          console.log(data);
+        link = baseUrl + "admin/ajax.php?script=eventGetGuests&eventId=" + eventId + "&projectId=" + projectId;
+        $.getJSON(link, function(data) {
+            console.log(data);
             var items = [];
             $.each(data, function(key, val) {
                 items.push("<li id='" + key + "'>" + val.name + "</li>");
             });
 
-            $("."+eventId+" .guestList").replaceWith("<ol>"+items.join('')+"</ol>");
+            $("." + eventId + " .guestList").replaceWith("<ol>" + items.join('') + "</ol>");
         });
     }
+
+    /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+    $('#topMenuHamburger').click(function() {
+        var x = document.getElementById("topMenu");
+        if (x.className === "topnav") {
+            x.className += " responsive";
+        } else {
+            x.className = "topnav";
+    }
+  });
+
 });
