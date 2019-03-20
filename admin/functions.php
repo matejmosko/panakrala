5d8a483f942a74c6fa9eef5c48b358a13cd2ab79
Matej Moško <matej.mosko@gmail.com>
2019-03-20 18:32:32 +0100

    Email event registration

--- admin/functions.php
+++ admin/functions.php
@@ -262,6 +262,21 @@ function eventSendEmail()
 {
 }
 
+function sendMessage($to, $subject, $txt)
+{
+    $from = $GLOBALS['data']['opts']['contactEmail'];
+
+    $headers = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+    $headers .= 'From: '.$GLOBALS['data']['opts']['emailFrom']. "\r\n";
+    $headers .= 'Cc: <'.$from.'>' . "\r\n";
+
+    if (mail($to, $subject, $txt, $headers)) {} else return "<p class='error'>Pri posielaní správy došlo k chybe. Skúste nám správu poslať priamo na ". $from."</p>";
+
+    //file_put_contents("email.txt", "To: ".$to."\n\n Subject: ".$subject."\n\n From: ".$from."\n\n Txt: ".$txt."\n\n Headers: ".$headers);
+}
+
 function adminEventGetGuests($eventId)
 {
     $conn = setupDB();
