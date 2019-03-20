c169ab1161d78f23cb70a99d7a2c8e41ab609821
Matej Moško <matej.mosko@gmail.com>
2019-03-20 18:44:42 +0100

    test

--- admin/functions.php
+++ admin/functions.php
@@ -265,6 +265,7 @@ function eventSendEmail()
 function sendMessage($to, $subject, $txt)
 {
     $from = $GLOBALS['data']['opts']['contactEmail'];
+    echo "text";
 
     $headers = "MIME-Version: 1.0" . "\r\n";
     $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
